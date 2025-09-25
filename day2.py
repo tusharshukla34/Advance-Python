b412209476e210989a7b061150978a865920a504
tusharshukla34 <tusharshukla880@gmail.com>
2025-09-25 19:38:40 +0530

    new

--- day2.py
+++ day2.py
@@ -14,3 +14,19 @@ arr = np.array([1,2,3,4,5])
 t4 = time.time()
 ta = t4 - t3
 print("array time :",ta)
+
+# %%
+"""
+
+1. for small datasets, Python lists often perform faster in creation time because
+   NumPy has setup overhead.
+
+2. For large datasets (thousands, millions, or more), NumPy arrays are much faster
+   for computation and memory efficiency because:
+
+   -- Operations are done in compiled C code.
+
+   -- Memory is contiguous, which improves CPU cache efficiency.
+
+"""
+# %%
